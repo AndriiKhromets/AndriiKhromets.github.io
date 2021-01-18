16e1fb68a9b678205da3eaa9b6ce3ebec6bae5cd
AndriiKhromets <andrii.khromets@gmail.com>
2021-01-18 18:51:26 +0200

    Practice-2 FINAL

--- js/Practice-2/js/script.js
+++ js/Practice-2/js/script.js
@@ -1,46 +1,60 @@
 // Завдання 1
+
 let name = "Ведіть своє імя";
 let test = prompt("name", '');
+
 // Завдання 2
+
 const ageMe = 12;
 const ageFrends = 12;
 alert (ageMe + ageFrends);
+
 // Завдання 3 
-let a = 1;
-let b = 1;
-console.log(a+b);
+
+let a = 10;
+let b = 15;
+console.log('a+b =', a+b);
 
 // Завдання 4
+
 let a1 = 5 + 3;
 let a2 = 5 - 3;
 let a3 = 5 * 3;
 let a4 = 5/3;
 
-console.log(Number(a1));
-console.log(Number(a2));
-console.log(Number(a3));
-console.log(Number(a4));
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+
 // Завдання 5
-let a5 = 75 + 'кг';
+
 let a6 = 5 % 3;
 let a7 = 3 % 5;
 let a8 = 5 + '3';
 let a9 = '5' - 3;
+let a10 = 75 + 'кг';
 
-alert(a5);
 alert(a6);
 alert(a7);
 alert(a8);
 alert(a9);
+alert(a10);
+
 // Завдання 6
-let height = 23;
-let width = 10;
-let s = height*widthж
+
+let height=23;
+let width=10;
+let s = height * width;
+
 // Завдання 7
+
 let heightC = 10
 let dC = 4
-let v = heightC*dC
+let v = heightC * dC
+
 // Завдання 8
-let n = 3;
-let m = 4;
-console.log(Math.pow(3, 4));
+
+let A = 4;
+let B = 3;
+let C = (4**2)+(3**2)
