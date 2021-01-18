518db2b2df6c59f7214ebdb0b5174007af14df8e
AndriiKhromets <andrii.khromets@gmail.com>
2021-01-18 18:10:07 +0200

    practice 1-2

---
+++ js/Practice-1/js/script.js
@@ -0,0 +1,20 @@
+let str = "Привіт";
+let num = 123;
+let flag = true;
+let txt = "true";
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof boolean);
+
+let house = "Величезний будинок";
+let name = "Ivan";
+let user = "people";
+let message = "Hello";
+let car = "AA 1234 BB";
+
+const hotel = "5 4 3 2 1";
+const confirm = "congratulations";
+const holiday = "New Year";
+const mouse = "small";
+const elephant = "big";
---
+++ js/Practice-2/js/script.js
@@ -0,0 +1,46 @@
+// Завдання 1
+let name = "Ведіть своє імя";
+let test = prompt("name", '');
+// Завдання 2
+const ageMe = 12;
+const ageFrends = 12;
+alert (ageMe + ageFrends);
+// Завдання 3 
+let a = 1;
+let b = 1;
+console.log(a+b);
+
+// Завдання 4
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5/3;
+
+console.log(Number(a1));
+console.log(Number(a2));
+console.log(Number(a3));
+console.log(Number(a4));
+// Завдання 5
+let a5 = 75 + 'кг';
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+
+alert(a5);
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+// Завдання 6
+let height = 23;
+let width = 10;
+let s = height*widthж
+// Завдання 7
+let heightC = 10
+let dC = 4
+let v = heightC*dC
+// Завдання 8
+let n = 3;
+let m = 4;
+console.log(Math.pow(3, 4));
