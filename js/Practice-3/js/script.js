be63887cab7b973ecf8014d7018528899d1fd532
AndriiKhromets <andrii.khromets@gmail.com>
2021-01-18 19:56:10 +0200

    Practice-3 FINAL

---
+++ js/Practice-3/js/script.js
@@ -0,0 +1,47 @@
+// Завдання 1
+
+// let k1 = Number.parseInt('5px'); 
+// let k2 = Number.parseInt('12djd334'); 
+// let k3 = Number.parseInt('12.45asdwe12'); 
+// let k4 = Number.parseInt('qwqweeewq'); 
+
+// Завдання 2
+
+// let k1 = Number.parseFloat('5px');
+// let k2 = Number.parseFloat('12djd334');
+// let k3 = Number.parseFloat('2.45asdwe12');
+// let k4 = Number.parseFloat('qwqweeewq');
+
+// Завдання 3
+
+// let = Math.max(2, 34, 99, 3, 22, 36, 733, 18);
+// let = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
+
+// Завдання 4
+
+// console.log(Math.random() * (3 - 19) + 1);
+
+// Завдання 5
+
+// let result = sum(5+5+'5');
+// console.log(result); 
+
+// Завдання 6
+
+// let email = "andrii.khromets@gmail.com";
+
+// console.log (email.length);
+// console.log (email.indexOF('@'));
+
+// Завдання 7
+// let one = "my";
+// let two = "name";
+// let three = "is";
+// const fullName = `${one} ${two} ${three} Victor .`;
+// console.log(fullName);
+
+// Завдання 8
+
+// let userName = "Олександра"
+// let payment = "300" 
+// alert(`Дякуємо,${userName}, До сплати  ${payment} гривень .`)
