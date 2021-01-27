3e3888d238252fb28e1a772d30e237c120db076d
AndriiKhromets <andrii.khromets@gmail.com>
2021-01-27 19:24:31 +0200

    Practice-1 FINAL

---
+++ js/Practice-4/js/script.js
@@ -0,0 +1,89 @@
+// Завдання 1
+
+// let today = 24;
+
+// if (today === 1) {
+// console.log(cогодні перше число); 
+// }
+// Завдання 2
+
+// let day = 24
+// const dayMonth = 24;
+
+// if (dayMonth%2==0) {
+//    console.log(day);
+// } else {
+//   console.log ("upsss")
+// }
+
+// Завдання 3
+
+// let cost = 5%2;
+//  if  (cost==0) {
+//   console.log(cost+"грн")
+//  } else {
+//    console.log(cost+"євро")
+//  }
+
+// Завдання 4
+
+// let age=55;
+// if (age>60) {
+//     console.log("Максим пенсіонер");
+//     } else if  (age<60 && age > 18){
+//     console.log("Максим дорослий");
+// }
+//     else if  (age<18){
+//         console.log("Максим дитина");
+//     }
+
+// Завдання 5
+
+// const one = (getRandomInt(2));
+// const two = (getRandomInt(2));
+
+// if (one==1 && two == 1) {
+//     console.log("ФАРТ");
+//     } else{
+//     console.log("Ти програв");
+// }
+
+// Завдання 6
+
+// const name = prompt('Ведіть ваше імя:')
+// const surname = prompt('Ведіть ваше прізвище:');
+
+// if (name.length>4 & surname.length>5){
+//     console.log(name.length + surname.length);
+// } else {
+//    console.log("УПС")
+// }
+
+// Завдання 7
+
+// let num = (getRandomInt(6))
+
+// if (num == 5) {
+//     console.log("число = 5")
+// }
+// else if (num == 4){
+//     console.log("число = 4")
+// }
+// else if (num == 3){
+//     console.log("число = 3")
+// }
+// else if (num == 2){
+//     console.log("число = 2")
+// }
+// else{
+//     console.log("число = 1")
+// }
+
+// Завдання 8
+
+// let MYage = "12"
+// let friendage = prompt('Ведіть вік вашого друга:');
+
+// if (friendage>12){
+//     alert(MYage.length + friendage.length)
+// }
