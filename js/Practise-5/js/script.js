2e08a192afdfb7634216b04138b87d628aa8d588
AndriiKhromets <andrii.khromets@gmail.com>
2021-02-03 19:44:53 +0200

    Practice-5 FINAL

---
+++ js/Practise-5/js/script.js
@@ -0,0 +1,31 @@
+// // Завдання 1
+// let counter = 0;
+
+// while (counter < 11) {
+//   console.log(counter);
+//   counter += 1;
+// }
+// // Завдання 2
+// const number = 10;
+
+// for (let i = 0; i <= number; i += 1) {
+//   if (i % 2 === 1) {
+//     continue;
+//   }
+
+//   console.log(i); 
+// }
+// // Завдання 3
+// let num = 0;
+
+// while (num <= 28) {
+//   console.log(num);
+//   num += 1;
+// }
+// Завдання 4
+// let name = '';
+
+// do {
+//   name = prompt('Ім’я вашого улюбленого героя', '');
+// } while (name.length < 6);
+// console.log(name);
