611e7c442c2bceabbfa4b7edd7a3558f641e7d3e
AndriiKhromets <andrii.khromets@gmail.com>
2021-02-10 18:00:21 +0200

    Practice-6 Final

---
+++ js/Practise-6/js/script.js
@@ -0,0 +1,44 @@
+// Завдання 1
+// const Arr1 = [1, 5, "4", "hello"];
+// const Arr2 = [true, 2, {}, ["a"], 222];
+
+// console.log(Arr1[2]);
+// console.log(Arr2[2]);
+// console.log(Arr1[2] + Arr2[2]);
+
+// Завдання 2
+
+// const Arr1 = [1, 5, "4", "hello"];
+// Arr1[4] = 22;
+
+// console.log(Arr1[0]);
+// console.log(Arr1[1]);
+// console.log(Arr1[2]);
+// console.log(Arr1[3]);
+// console.log(Arr1[4]);
+
+// Завдання 3
+
+// const Arr2 = [true, 2, {}, ["a"], 222];
+
+// for (const arr2 of Arr2) {
+//     console.log(arr2);
+//     console.log(typeof arr2);
+// }
+
+// Завдання 4
+
+// const message = 'Welcome to Ukraine!';
+// const letters = message.split('');
+// console.log(letters);
+// console.log(letters.indexOf("l"));
+// console.log(letters.join(''));
+
+// Завдання 5
+
+// const styles = ["Джаз", "Блюз"];
+// styles.push("Рок-н-ролл");
+// styles.splice(2, 0, "Класика");
+// console.log(styles.shift());
+// styles.unshift("Реп", "Реггі");
+// console.log(styles);
