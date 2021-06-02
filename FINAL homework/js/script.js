5c4cbe54b424cbe2c77d1450db8e9eb07e23cca2
AndriiKhromets <andrii.khromets@gmail.com>
2021-06-02 19:37:38 +0300

    Final homework

--- FINAL homework/js/script.js
+++ FINAL homework/js/script.js
@@ -1,64 +1,56 @@
-let buttonAdd = document.querySelector(".btn"),
-createCard = document.querySelector(".create_todo");
+const buttonAdd = document.querySelector(".btn");
+const buttonSave = document.getElementById("save");
+const todoEditor = document.querySelector(".js-todo-editor");
+const cardlistInTodo = document.querySelector(".card_list_todo");
+const cardlistInProgress = document.querySelector(".card_list_inprogress");
+const cardlistDone = document.querySelector(".card_list_done");
+console.log(cardlistInTodo);
+console.log(cardlistInProgress, cardlistDone)
 
-let buttonSave = document.getElementById("save"),
-    inputName = document.getElementById("name"),
-    inputDescription = document.getElementById("description"),
-    buttonsRadio = document.querySelectorAll(".radio_btn");
+Sortable.create(cardlistInTodo, { group: "board" });
+Sortable.create(cardlistInProgress, { group: "board" });
+Sortable.create(cardlistDone, { group: "board" });
 
+buttonAdd.addEventListener("click", showTodoEditor);
+todoEditor.addEventListener("submit", addCard);
 
-let data = {
-    name:"",
-    description:"",
-    priority:""
-};
+const crossIcon = `
+    <svg class='cross' width='32' height='32' fill='currentColor' stroke='currentColor'  viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>    <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2'  d='M6 18L18 6M6 6l12 12'></path>  </svg>   
+    `;
 
-buttonSave.addEventListener("click", (event)=>{
-    event.preventDefault();
-    data.name = inputName.value;
-    data.description = inputDescription.value;
+function createCard(title, description, priority) {
+    let card = document.createElement("li");
+    card.classList.add("card");
+    card.innerHTML = `<h5>${title}</h5><p>${description}</p>${crossIcon}`;
+    card.setAttribute("priority", priority);
+    if (priority === "H"){
+        card.style.background = "blueviolet";
+    } else if (priority === "M") {
+        card.style.background= "yellowgreen"; 
+        } else if (priority === "L") {
+        card.style.background="pink";
+    }
 
-    clearInputs(inputName, inputDescription);
-    createCard.style.display="nove";
-    arrowToDo = cardlistTodo.querySelectorAll(".arrow");
-    console.log(arrowToDo)
-}); 
-
-buttonsRadio.forEach((button)=>{
-    button.addEventListener("change", function(){
-        console.log(button);
-        data.priority = button.getAttribute("priority");
-    })
-});
-
-
-
-function createCardFunction(buttonSave,fcardlistTodo);
-function clearInputs(nameInput,descriptionInput){
-    nameInput.value="";
-    descriptionInput.value ="";
+    cardlistInTodo.append(card);
 }
 
-let cardlistInProgress = document.querySelector(".card_list_inprogress"),
-arrowToDo = cardlistTodo.querySelectorAll(".arrow");
-console.log(arrowToDo);
+function showTodoEditor() {
+    todoEditor.classList.add("is-visible");
+}
 
-buttonAdd.addEventListener("click", function(){
-    createCard.style.display="block";
-});
+function hideTodoEditor() {
+    todoEditor.classList.remove("is-visible");
+}
 
-function createCardFunction(button, list){
-    button.addEventListener("click", function(){
-        let card = document.createElement("li");
-        card.classList.add("card");
-        card.innerHTML= " <h5>"+data.name+"</h5>  <p>"+data.description+"</p>  <svg class='cross' width='32' height='32' fill='currentColor' stroke='currentColor'  viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>    <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2'  d='M6 18L18 6M6 6l12 12'></path>  </svg>   <svg xmlns='http://www.w3.org/2000/svg' class='arrow_right' width='32' height='32'   fill='currentColor' viewBox='0 0 24 24' stroke='currentColor'>   <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M17 8l4 4m0 0l-4 4m4-4H3'/> </svg>"
-        if(data.priority === "H"){
-            card.style.background==="blueviolet";
-        } else if (data.priority === "M") {
-            card.style.background=== "yellowgreen"; 
-        } else if (data.priority === "L") {
-            card.style.background==="pink";
-        }
-        list.innertBefore(card,createCard);
-    })
+function addCard(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const title = formElements.title.value;
+    const description = formElements.description.value;
+    const priority = formElements.priority.value;
+    console.log(formElements);
+
+    createCard(title, description, priority);
+    event.currentTarget.reset();
+    hideTodoEditor();
 }
