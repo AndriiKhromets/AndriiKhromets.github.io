8f57a9187ddaedf22f98cd50c8f35749ebaca563
AndriiKhromets <andrii.khromets@gmail.com>
2022-01-06 19:09:01 +0200

    end py 1

---
+++ Python/own-finances-bot-main/step02/02bot_echo.py
@@ -0,0 +1,13 @@
+# from telegram.ext import Updater, MessageHandler, Filters
+
+# def echo(update, context):
+#     print(update.message.text)
+#     update.message.reply_text(update.message.text)
+
+# updater = Updater("5075490510:AAHsP0b2sCoGRK4jYGX1WAtifVItIvPqMYI")
+# dispatcher = updater.dispatcher
+
+# dispatcher.add_handler(MessageHandler(Filters.all, echo))
+
+# updater.start_polling()
+# updater.idle()
